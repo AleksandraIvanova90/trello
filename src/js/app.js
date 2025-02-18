48581c03afb64a16ba2e22b6e73004af7af977d1
Aleksandra Ivanova <sashaivanova90@mail.ru>
2025-02-18 15:10:46 +0300

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+import Card from './card';
+import Column from './column';
+import locStorage from './locStorage';
+// localStorage.clear()
+locStorage('.column');
+
+window.onload = function() {
+    const column = new Column('.columns');
+
+    const card = new Card('.columns');
+}
+
+
